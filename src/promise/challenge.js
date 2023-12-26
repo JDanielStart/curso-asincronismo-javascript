676e3b14ba8d147c9e1f44251ad083b091b707c8
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-12-26 19:03:33 +0000

    Consumir api con promesas usando fetchData

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -5,9 +5,27 @@ function fetchData(urlAPI) {
     return fetch(urlAPI);
 };
 
-fetchData(`${API}/products`)
+/* fetchData(`${API}/products`)
 .then(response => response.json())
 .then(products => {
     console.log(products);
 })
-.catch(error => console.log(error));
+.catch(error => console.log(error)); */
+
+fetchData(`${API}/products`)
+.then(response => response.json())
+.then(products => {
+    console.log(products);
+    return fetchData(`${API}/products/${products[0].id}`);
+})
+.then(response => response.json())
+.then(product => {
+    console.log(product.title);
+    return fetchData(`${API}/categories/${product.category.id}`)
+})
+.then(response => response.json())
+.then(category => {
+    console.log(category.name);
+})
+.catch(error => console.error(error))
+.finally( () => console.log("Finally"));
